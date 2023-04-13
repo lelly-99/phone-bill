ff4fea9d514286a749a8bd51619fb60af0c8bc90
progress <leswifil@gmail.com>
2023-04-13 22:32:18 +0200

    calculate bill

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,13 +1,34 @@
 //get a reference to the calculate button
-
+var billString = document.querySelector(".billString")
 //get a reference to the billTotal element
-
+var calculateBtn = document.querySelector(".calculateBtn")
 //get a reference to the billString
-
+var billTotal = document.querySelector(".billTotal")
 //create the function that will be called when the calculate button is pressed
 //  * this function should read the string value entered - split it on a comma.
 //  * loop over all the entries in the the resulting list
 //  * check if it is a call or an sms and add the right amount to the overall total
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
-
-//link the function to a click event on the calculate button
+function totalPhoneBill() {
+    let bill = billString.value.toLowerCase();
+    var phoneBill = bill.split(",");
+    var total = 0;
+    for (var i = 0; i < phoneBill.length; i++) {
+        if (phoneBill[i].trim() === "call") {
+            total += 2.75;
+        }
+        else if (phoneBill[i].trim() === "sms") {
+            total += 0.75;
+        }
+        if (total > 20) {
+            billTotal.classList.add("warning");
+        }
+        if (total > 30) {
+            billTotal.classList.add("danger");
+        }
+    }
+    billTotal.innerHTML = total.toFixed(2);;
+}
+calculateBtn.addEventListener("click", function(){
+    totalPhoneBill();
+});
