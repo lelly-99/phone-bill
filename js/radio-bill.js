dcfc88ee88833f56b2525bd8b96b2eb292aab8c4
progress <leswifil@gmail.com>
2023-04-17 10:57:20 +0200

    radio bill done

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,37 +1,48 @@
 // get a reference to the sms or call radio buttons
-
+var billItemTypeRadio = document.querySelector(".billItemType")
 //get a reference to the add button
-
+var radioBillAddBtn = document.querySelector(".radioBillAddBtn")
 //create a variable that will keep track of the total bill
+var smsTotalTwo = document.querySelector(".smsTotalTwo")
+var callTotalTwo =document.querySelector(".callTotalTwo")
+var totalTwo = document.querySelector(".totalTwo")
 
+var totalSms = 0
+var totalCall = 0
+var costTotal = 0
 //add an event listener for when the add button is pressed
 
 //in the event listener get the value from the billItemTypeRadio radio buttons
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
-function textBillTotal(){
-    // get the value entered in the billType textfield
-    var billTypeEntered = billTypeText.value.trim();
-    // update the correct total
-    if (billTypeEntered === "bill"){
-        callsTotal += 2.75
+function calculateRadioBill() {
+    var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
+    if (checkedRadioBtn){
+        var billItemType = checkedRadioBtn.value
     }
-    else if (billTypeEntered === "sms"){
-        smsTotal += 0.75;
+    if (billItemType === "call") {
+        totalCall += 2.75;
     }
-    //update the totals that is displayed on the screen.
-    callsTotalElem.innerHTML = callsTotal.toFixed(2);
-    smsTotalElem.innerHTML = smsTotal.toFixed(2);
-    var totalCost = callsTotal + smsTotal;
-    totalCostElem.innerHTML = totalCost.toFixed(2);
-     //color the total based on the criteria
-    if (totalCost >= 50){
-        // adding the danger class will make the text red
-        totalCostElem.classList.add("danger");
+    else if (billItemType === "sms") {
+            totalSms += 0.75;
     }
-    else if (totalCost >= 30){
-        totalCostElem.classList.add("warning");
+    else {
+        costTotal += 0
+    }
+
+    costTotal = totalCall + totalSms;
+    callTotalTwo.innerHTML = totalCall.toFixed(2);
+    smsTotalTwo.innerHTML = totalSms.toFixed(2);
+    totalTwo.innerHTML = costTotal.toFixed(2);
+
+    if (costTotal > 30) {
+        totalTwo.classList.add("warning");
+    }
+    if (costTotal > 50) {
+        totalTwo.classList.add("danger");
     }
 }
-textTotalAddBtn.addEventListener('click', textBillTotal);
+radioBillAddBtn.addEventListener("click", function(){
+    calculateRadioBill();
+});
